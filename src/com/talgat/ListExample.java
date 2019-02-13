dda3f74672e396b26122ca1073bed768a4efdc64
Talgat Akunsartov <talgat.akunsartov@gmail.com>
2019-02-13 11:40:16 +0600

    List example start

---
+++ src/com/talgat/ListExample.java
@@ -0,0 +1,15 @@
+package com.talgat;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class ListExample {
+
+    public static void main(String[] args) {
+        List<Number> list = new ArrayList<>();
+        list.add(1);
+
+        List<Number> list1 = new LinkedList<>();
+    }
+}
